ffaa52b93d993ad59bdad31da6c663be4c786bb4
James Gore <73591234+jgore077@users.noreply.github.com>
2024-03-16 19:17:00 -0400

    added no sandbox for docker

--- index.js
+++ index.js
@@ -201,6 +201,8 @@ const getListVideoByUsername = async (username,snipe) => {
   
     const browser = await puppeteer.launch({
         headless: true,
+        args:['--no-sandbox']
+        
     })
     
     const page = await browser.newPage()
